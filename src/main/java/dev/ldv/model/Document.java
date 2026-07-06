6e006c1f4ead6c33dad4c65db42859a729bef4e6
DmitryLoginov <daimom25@gmail.com>
2026-07-06 14:01:39 +0300

    UPGDL-7. Добавить геттеры и сеттеры в @Embeddable-классы

--- src/main/java/dev/ldv/model/Document.java
+++ src/main/java/dev/ldv/model/Document.java
@@ -2,8 +2,14 @@ package dev.ldv.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Embeddable
+@Getter
+@Setter
+@ToString
 public class Document {
     @Column(name = "document_number", nullable = false)
     private String documentNumber;
--- src/main/java/dev/ldv/model/Name.java
+++ src/main/java/dev/ldv/model/Name.java
@@ -2,8 +2,14 @@ package dev.ldv.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Embeddable
+@Getter
+@Setter
+@ToString
 public class Name {
     @Column(name = "first_name", nullable = false)
     private String firstName;
