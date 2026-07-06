3b5877396cac3981e844fcabfae58909c539d509
DmitryLoginov <daimom25@gmail.com>
2026-07-06 14:02:51 +0300

    UPGDL-7. Добавить query-методы в ClientRepository

--- src/main/java/dev/ldv/repository/ClientRepository.java
+++ src/main/java/dev/ldv/repository/ClientRepository.java
@@ -1,10 +1,13 @@
 package dev.ldv.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import dev.ldv.model.Client;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ClientRepository extends JpaRepository<Client, UUID> {
+public interface ClientRepository extends JpaRepository<Client, UUID>, JpaSpecificationExecutor<Client> {
+    Optional<Client> findByMdmCode(Long mdmCode);
 }
