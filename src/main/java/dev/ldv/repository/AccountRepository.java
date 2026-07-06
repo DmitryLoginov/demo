c4108b28f5d88593dee797bd09b32309b1310144
DmitryLoginov <daimom25@gmail.com>
2026-07-06 14:03:03 +0300

    UPGDL-7. Добавить query-методы в AccountRepository

--- src/main/java/dev/ldv/repository/AccountRepository.java
+++ src/main/java/dev/ldv/repository/AccountRepository.java
@@ -1,10 +1,13 @@
 package dev.ldv.repository;
 
-import dev.ldv.model.Account;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.ldv.model.Account;
 
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+    boolean existsByClientId(UUID clientId);
+
+    boolean existsByClientIdAndStatusName(UUID clientId, String statusName);
 }
