26ef86d7450902cccc95a56b718e0b01a4ffa9f7
DmitryLoginov <daimom25@gmail.com>
2026-07-07 20:57:19 +0300

    UPGDL-7. Добавить toString() в ClientFilter

--- src/main/java/dev/ldv/dto/ClientFilter.java
+++ src/main/java/dev/ldv/dto/ClientFilter.java
@@ -1,9 +1,13 @@
 package dev.ldv.dto;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import dev.ldv.api.model.ClientStatus;
+import dev.ldv.utils.ObjectMapperUtils;
 
 @Getter
 @Setter
@@ -12,9 +16,18 @@ import lombok.Setter;
 public class ClientFilter {
     private int page = 0;
     private int size = 20;
-    private String lastName;
     private String firstName;
+    private String lastName;
     private String middleName;
-    private String status;
+    private ClientStatus status;
     private Long mdmCode;
+
+    @Override
+    public String toString() {
+        try {
+            return ObjectMapperUtils.writeValueAsString(this);
+        } catch (JsonProcessingException jsonProcessingException) {
+            return super.toString();
+        }
+    }
 }
