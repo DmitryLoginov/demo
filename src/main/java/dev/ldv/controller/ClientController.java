4ec16cafabd7ffdc69f3aba21351f92a34943ce8
DmitryLoginov <daimom25@gmail.com>
2026-07-07 16:38:43 +0300

    UPGDL-7. Переписать ClientController для использования сгенерированных dto и имплементации сгенерированного api-интерфейса

--- src/main/java/dev/ldv/controller/ClientController.java
+++ src/main/java/dev/ldv/controller/ClientController.java
@@ -4,11 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import dev.ldv.dto.*;
-import dev.ldv.exception.ApiException;
-import dev.ldv.exception.ApiError;
+import dev.ldv.api.ApiClient;
+import dev.ldv.api.model.*;
+import dev.ldv.dto.ClientFilter;
 import dev.ldv.service.ClientService;
 
 import java.util.UUID;
@@ -16,17 +17,14 @@ import java.util.UUID;
 @RestController
 @RequestMapping("api/v1/clients")
 @Slf4j
+@Validated
 @RequiredArgsConstructor
-public class ClientController {
+public class ClientController implements ApiClient {
     private final ClientService clientService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ClientDto create(@RequestHeader("Content-Type") String contentType,
-                            @RequestHeader("Accept") String accept,
-                            @RequestBody @Valid NewClientRequest newClientRequest) {
-        validateHeaders(contentType, accept);
-
+    public ClientDto create(@RequestBody @Valid NewClientRequest newClientRequest) {
         log.debug("POST /api/v1/clients");
         log.debug("Request body: {}", newClientRequest);
 
@@ -34,21 +32,21 @@ public class ClientController {
     }
 
     @GetMapping("/{clientId}")
-    public ClientDto getById(@RequestHeader("Content-Type") String contentType,
-                             @RequestHeader("Accept") String accept,
-                             @PathVariable UUID clientId) {
-        validateHeaders(contentType, accept);
-
+    public ClientDto getById(@PathVariable UUID clientId) {
         log.debug("GET /api/v1/clients/{}", clientId);
 
         return clientService.getById(clientId);
     }
 
     @GetMapping
-    public PageResponse<ClientShortDto> getPage(@RequestHeader("Content-Type") String contentType,
-                                                @RequestHeader("Accept") String accept,
-                                                ClientFilter clientFilter) {
-        validateHeaders(contentType, accept);
+    public ClientPageResponse getPage(@RequestParam(required = false, defaultValue = "0") Integer page,
+                                      @RequestParam(required = false, defaultValue = "20") Integer size,
+                                      @RequestParam(required = false) String firstName,
+                                      @RequestParam(required = false) String lastName,
+                                      @RequestParam(required = false) String middleName,
+                                      @RequestParam(required = false) String status,
+                                      @RequestParam(required = false) Long mdmCode) {
+        ClientFilter clientFilter = new ClientFilter(page, size, firstName, lastName, middleName, status, mdmCode);
 
         log.debug("GET /api/v1/clients");
         log.debug("Query params: {}", clientFilter);
@@ -57,12 +55,8 @@ public class ClientController {
     }
 
     @PutMapping("/{clientId}")
-    public ClientDto update(@RequestHeader("Content-Type") String contentType,
-                            @RequestHeader("Accept") String accept,
-                            @PathVariable UUID clientId,
+    public ClientDto update(@PathVariable UUID clientId,
                             @RequestBody @Valid UpdateClientRequest updateClientRequest) {
-        validateHeaders(contentType, accept);
-
         log.debug("PUT /api/v1/clients/{}", clientId);
         log.debug("Request body: {}", updateClientRequest);
 
@@ -71,36 +65,16 @@ public class ClientController {
 
     @DeleteMapping("/{clientId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(@RequestHeader("Content-Type") String contentType,
-                           @RequestHeader("Accept") String accept,
-                           @PathVariable UUID clientId) {
-        validateHeaders(contentType, accept);
-
+    public void deleteById(@PathVariable UUID clientId) {
         log.debug("DELETE /api/v1/clients/{}", clientId);
 
         clientService.deleteById(clientId);
     }
 
     @GetMapping("/{clientId}/exists")
-    public ClientExistenceDto exists(@RequestHeader("Content-Type") String contentType,
-                                     @RequestHeader("Accept") String accept,
-                                     @PathVariable UUID clientId) {
-        validateHeaders(contentType, accept);
-
+    public ClientExistenceDto exists(@PathVariable UUID clientId) {
         log.debug("GET /api/v1/clients/{}/exists", clientId);
 
         return clientService.exists(clientId);
-    }
-
-    private void validateHeaders(String contentType, String accept) {
-        if (!contentType.equals("application/json")) {
-            throw new ApiException(ApiError.BAD_REQUEST,
-                    "Invalid content type: " + contentType + ". Expecting application/json");
-        }
-
-        if (!accept.equals("application/json")) {
-            throw new ApiException(ApiError.BAD_REQUEST,
-                    "Invalid accept type: " + accept + ". Expecting application/json");
-        }
     }
 }
