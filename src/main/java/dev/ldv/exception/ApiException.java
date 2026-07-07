f8c2c844001ab0db94bfd8ff066fb71ece1fdac5
DmitryLoginov <daimom25@gmail.com>
2026-07-07 09:49:47 +0300

    UPGDL-7. Скорректировать ApiException

--- src/main/java/dev/ldv/exception/ApiException.java
+++ src/main/java/dev/ldv/exception/ApiException.java
@@ -5,10 +5,9 @@ import lombok.Getter;
 @Getter
 public class ApiException extends RuntimeException {
     private final ApiError apiError;
-    private final String errorDescription;
 
-    public ApiException(ApiError apiError, String errorDescription) {
+    public ApiException(ApiError apiError, String message) {
+        super(message);
         this.apiError = apiError;
-        this.errorDescription = errorDescription;
     }
 }
